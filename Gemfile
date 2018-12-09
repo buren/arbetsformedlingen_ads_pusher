4ea77d7ce2edaac5f2a029f3ed276dd6120e0ec0
Jacob Burenstam <burenstam@gmail.com>
2018-12-09 14:38:32 +0100

    Remove stuff we don't need

--- Gemfile
+++ Gemfile
@@ -4,10 +4,9 @@ gem 'sinatra'
 gem 'json'
 gem 'rack'
 gem 'rack-contrib'
-gem 'aws-record'
 
 # These are the dependencies that are used only for unit tests.
 group :test do
-  gem "rspec"
-  gem "rack-test"
+  gem 'rspec'
+  gem 'rack-test'
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -4,7 +4,7 @@ require 'rack/test'
 set :environment, :test
 
 # Tests for server.rb
-describe 'HelloWorld Service' do
+describe 'Server' do
   include Rack::Test::Methods
 
   def app
@@ -12,18 +12,18 @@ describe 'HelloWorld Service' do
   end
 
   # Test for HTTP GET for URL-matching pattern '/'
-  it "should return successfully on GET" do
+  it 'should return successfully on GET' do
     get '/hello-world'
     expect(last_response).to be_ok
     json_result = JSON.parse(last_response.body)
-    expect(json_result["Output"]).to eq("Hello World!")
+    expect(json_result['output']).to eq('Hello World!')
   end
 
   # Test for HTTP POST for URL-matching pattern '/'
-  it "should return successfully on POST" do
+  it 'should return successfully on POST' do
     post '/hello-world'
     expect(last_response).to be_ok
     json_result = JSON.parse(last_response.body)
-    expect(json_result["Output"]).to eq("Hello World!")
+    expect(json_result['output']).to eq('Hello World!')
   end
 end
